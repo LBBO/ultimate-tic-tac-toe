6ea02442e4d5e1cc35d0e7753fa5f47ba9f5af29
Michael Kuckuk <mdk@kuckuk.com>
2018-09-09 18:13:31 +0200

    Add more tests for FieldOfGamefields

--- test/FieldOfGamefields_test.js
+++ test/FieldOfGamefields_test.js
@@ -37,9 +37,12 @@ describe('Class FieldOfGamefields', () => {
 		});
 		
 		it('activeSingleGamefields to an array containing all SingleGamefields indexes', () => {
-			expect(fieldOfGamefields.activeSingleGamefields).to.have.lengthOf(fieldOfGamefields.numberOfRows * fieldOfGamefields.numberOfCols);
+			expect(fieldOfGamefields.activeSingleGamefields)
+				.to
+				.have
+				.lengthOf(fieldOfGamefields.numberOfRows * fieldOfGamefields.numberOfCols);
 			
-			for(let i = 0; i < fieldOfGamefields.activeSingleGamefields.length; i++) {
+			for (let i = 0; i < fieldOfGamefields.activeSingleGamefields.length; i++) {
 				expect(fieldOfGamefields.activeSingleGamefields[i]).to.equal(i);
 			}
 		});
@@ -89,6 +92,11 @@ describe('Class FieldOfGamefields', () => {
 			expect(fieldOfGamefields.CheckIfMoveIsValid(0, 0)).to.be.false;
 		});
 		
+		it('Should return false if SingleGamefield has been won', () => {
+			fieldOfGamefields.field[0][0].isWon = true;
+			expect(fieldOfGamefields.CheckIfMoveIsValid(0, 0)).to.be.false;
+		});
+		
 		it('Should return false if any index is out of bound', () => {
 			const maxRows = fieldOfGamefields.numberOfRows;
 			const maxCols = fieldOfGamefields.numberOfCols;
@@ -112,8 +120,9 @@ describe('Class FieldOfGamefields', () => {
 			}
 		});
 		
-		it('Should return true if game has not been won, targeted tile exists and is active', () => {
+		it('Should return true if game has not been won, targeted tile exists, is active and has not been won', () => {
 			fieldOfGamefields.activeSingleGamefields = [0];
+			fieldOfGamefields.field[0][0].isWon = false;
 			fieldOfGamefields.isWon = false;
 			expect(fieldOfGamefields.CheckIfMoveIsValid(0, 0)).to.be.true;
 		});
@@ -156,73 +165,188 @@ describe('Class FieldOfGamefields', () => {
 			checkIfMoveIsValidStub.restore();
 		});
 		
-		xit('Should check if move is valid before making move', () => {
+		it('Should calculate row and col from both totalIndexes when called with two numbers', () => {
+			let rowAndColFromTotalIndex = sinon.spy(fieldOfGamefields, 'RowAndColFromTotalIndex');
+			let makeMove = sinon.spy(fieldOfGamefields, 'MakeMove');
+			
+			expect(rowAndColFromTotalIndex).to.not.have.been.called;
+			fieldOfGamefields.MakeMove(0, 0);
+			expect(rowAndColFromTotalIndex).to.have.been.called;
+			expect(makeMove).to.have.been.calledWithExactly(0, 0, 0, 0);
+			
+			fieldOfGamefields.MakeMove(2, 3);
+			expect(makeMove).to.have.been.calledWithExactly(0, 2, 1, 0);
+			
+			rowAndColFromTotalIndex.restore();
+			makeMove.restore();
+		});
+		
+		describe('Should print warning to console and return false when called with wrong amount or types of' +
+					 ' arguments', () => {
+			const wrongArgs = [
+				['0'],
+				[0],
+				[NaN],
+				[NaN, 0],
+				[0, NaN],
+				[NaN, NaN],
+				[NaN, 0, 0, 0],
+				[0, NaN, 0, 0],
+				[0, 0, NaN, 0],
+				[0, 0, 0, NaN],
+				[NaN, NaN, NaN, NaN],
+				[1, 2, 3, 4, 5],
+				[]
+			];
+			
+			wrongArgs.forEach(args => {
+				it('Arguments: ' + args.toString(), () => {
+					const consoleWarnStub = sinon.stub(console, 'warn');
+					expect(consoleWarnStub).to.not.have.been.called;
+					
+					//either console.warn is called or an error is thrown
+					//todo make this constistent
+					try {
+						expect(() => fieldOfGamefields.MakeMove(...args)).to.throw();
+					} catch (e) {
+						expect(consoleWarnStub).to.have.been.called;
+					}
+					
+					consoleWarnStub.restore();
+				});
+			});
+		});
+		
+		it('Should check if move is valid before making move', () => {
 			checkIfMoveIsValidStub.returns(false);
 			
 			expect(checkIfMoveIsValidStub).to.not.have.been.called;
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerX);
+			fieldOfGamefields.MakeMove(0, 0);
 			expect(checkIfMoveIsValidStub).to.have.been.calledOnce;
 		});
 		
-		xit('Should set targeted field to specified player if move is valid', () => {
+		it('Should call SingleGamefield.MakeMove if move is valid', () => {
 			checkIfMoveIsValidStub.returns(true);
+			let makeMoveSpy = sinon.spy(fieldOfGamefields.field[0][0], 'MakeMove');
 			
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerX);
-			expect(fieldOfGamefields.field[0][0]).to.equal(fieldOfGamefields.playerX);
+			expect(makeMoveSpy).to.not.have.been.called;
+			fieldOfGamefields.MakeMove(0, 0);
+			expect(makeMoveSpy).to.have.been.calledOnce;
 			
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerO);
-			expect(fieldOfGamefields.field[0][0]).to.equal(fieldOfGamefields.playerO);
+			makeMoveSpy.restore();
 		});
 		
-		xit('Should not set targeted field to specified player if move is not valid', () => {
+		it('Should not call SingleGamefield.MakeMove if move is not valid', () => {
 			checkIfMoveIsValidStub.returns(false);
-			fieldOfGamefields.field[0][0] = fieldOfGamefields.nobody;
+			let makeMoveSpy = sinon.spy(fieldOfGamefields.field[0][0], 'MakeMove');
 			
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerX);
-			expect(fieldOfGamefields.field[0][0]).to.equal(fieldOfGamefields.nobody);
+			expect(makeMoveSpy).to.not.have.been.called;
+			fieldOfGamefields.MakeMove(0, 0);
+			expect(makeMoveSpy).to.not.have.been.called;
 			
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerO);
-			expect(fieldOfGamefields.field[0][0]).to.equal(fieldOfGamefields.nobody);
+			makeMoveSpy.restore();
 		});
 		
-		xit('Should increment occupiedFields on every call if move is valid', () => {
+		it('Should call checkForWin and increment occupiedFields if move is valid and SingleGamefield has been' +
+			   ' won', () => {
 			checkIfMoveIsValidStub.returns(true);
-			
-			for (let i = 1; i < 20; i++) {
-				fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerO);
-				expect(fieldOfGamefields.occupiedFields).to.equal(i);
-			}
-		});
-		
-		xit('Should not increment occupiedFields on every call if move is not valid', () => {
-			checkIfMoveIsValidStub.returns(false);
-			
-			for (let i = 1; i < 20; i++) {
-				fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerO);
-				expect(fieldOfGamefields.occupiedFields).to.equal(0);
-			}
-		});
-		
-		xit('Should check for win if move is valid', () => {
-			checkIfMoveIsValidStub.returns(true);
-			const checkForWinSpy = sinon.spy(fieldOfGamefields, 'checkForWin');
+			let checkForWinSpy = sinon.spy(fieldOfGamefields, 'checkForWin');
+			let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[0][0], 'CheckIfMoveIsValid').returns(true);
+			fieldOfGamefields.field[0][0].isWon = true;
 			
 			expect(checkForWinSpy).to.not.have.been.called;
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerX);
+			fieldOfGamefields.MakeMove(0, 0);
 			expect(checkForWinSpy).to.have.been.calledOnce;
 			
 			checkForWinSpy.restore();
+			checkInnerMoveIsValid.restore();
 		});
 		
-		xit('Should not check for win if move is not valid', () => {
+		it('Should not call checkForWin and increment occupiedFields if move is not valid and SingleGamefield has' +
+			   ' been won', () => {
 			checkIfMoveIsValidStub.returns(false);
-			const checkForWinSpy = sinon.spy(fieldOfGamefields, 'checkForWin');
+			let checkForWinSpy = sinon.spy(fieldOfGamefields, 'checkForWin');
+			let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[0][0], 'CheckIfMoveIsValid').returns(true);
+			fieldOfGamefields.field[0][0].isWon = true;
 			
 			expect(checkForWinSpy).to.not.have.been.called;
-			fieldOfGamefields.MakeMove(0, 0, fieldOfGamefields.playerX);
+			fieldOfGamefields.MakeMove(0, 0);
 			expect(checkForWinSpy).to.not.have.been.called;
 			
 			checkForWinSpy.restore();
+			checkInnerMoveIsValid.restore();
+		});
+		
+		it('Should increment occupiedFields on every call if move is valid and SingleGamefield has been won', () => {
+			checkIfMoveIsValidStub.returns(true);
+			let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[0][0], 'CheckIfMoveIsValid').returns(true);
+			fieldOfGamefields.field[0][0].isWon = true;
+			
+			for (let i = 1; i < 20; i++) {
+				fieldOfGamefields.MakeMove(0, 0);
+				expect(fieldOfGamefields.occupiedFields).to.equal(i);
+			}
+			
+			checkInnerMoveIsValid.restore();
+		});
+		
+		it('Should not increment occupiedFields on every call if move is not valid and SingleGamefield has been won', () => {
+			checkIfMoveIsValidStub.returns(false);
+			let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[0][0], 'CheckIfMoveIsValid').returns(true);
+			fieldOfGamefields.field[0][0].isWon = true;
+			
+			for (let i = 1; i < 20; i++) {
+				fieldOfGamefields.MakeMove(0, 0);
+				expect(fieldOfGamefields.occupiedFields).to.equal(0);
+			}
+			
+			checkInnerMoveIsValid.restore();
+		});
+		
+		describe('Should set activeSingleGamefields correctly when SingleGamefield has not been won', () => {
+			for (let i = 0; i < 9; i++) {
+				it('When move is made in totalIndex ' + i, () => {
+					checkIfMoveIsValidStub.returns(true);
+					const {row, col} = fieldOfGamefields.RowAndColFromTotalIndex(i);
+					let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[row][col], 'CheckIfMoveIsValid')
+						.returns(true);
+					fieldOfGamefields.field[row][col].isWon = false;
+					
+					fieldOfGamefields.MakeMove(i, i);
+					expect(fieldOfGamefields.activeSingleGamefields).to.deep.equal([i]);
+					
+					checkInnerMoveIsValid.restore();
+				});
+			}
+		});
+		
+		describe('Should set activeSingleGamefields correctly when SingleGamefield has been won', () => {
+			const checkForNWonFields = n => {
+				it('And ' + n + ' other SingleGamefields have been won', () => {
+					checkIfMoveIsValidStub.returns(true);
+					let checkInnerMoveIsValid = sinon.stub(fieldOfGamefields.field[0][0], 'CheckIfMoveIsValid')
+						.returns(true);
+					fieldOfGamefields.field[0][0].isWon = true;
+					
+					for(let i = 1; i <= n; i++) {
+						const {row, col} = fieldOfGamefields.RowAndColFromTotalIndex(i);
+						fieldOfGamefields.field[row][col].isWon = true;
+					}
+					
+					fieldOfGamefields.MakeMove(0, 0);
+					expect(fieldOfGamefields.activeSingleGamefields).to.deep.equal(
+						new Array(9 - n - 1)
+							.fill(0)
+							.map((_, index) => index + n + 1)
+					);
+					
+					checkInnerMoveIsValid.restore();
+				});
+			};
+			
+			for (let i = 0; i < 8; i++) {
+				checkForNWonFields(i);
+			}
 		});
 	});
 });
