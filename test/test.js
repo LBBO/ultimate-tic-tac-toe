8aa95d60eb68fa4668374026261a536e88c6d098
Michael David Kuckuk <mdk@kuckuk.com>
2018-09-09 18:23:41 +0200

    Add tests for and fix FieldOfGamefields (#1)
    * Add tests for FieldOfGamefields (which now allegedly has 100% coverage)
    
    * Fix FieldOfGamefields so it passes all tests

--- test/test.js
+++ test/test.js
@@ -7,4 +7,5 @@ Enzyme.configure({ adapter: new Adapter() });
 // You can list more individual test files here:
 require("Gamefield_test");
 require("SingleGamefield_test");
+require("FieldOfGamefields_test");
 //require("app_test");
