37f9045971490ec85217f5881dc1a80e60271356
Michael Kuckuk <mdk@kuckuk.com>
2020-04-23 16:04:28 +0200

    Remove jekyll theme

--- source/entry.jsx
+++ source/entry.jsx
@@ -1,6 +1,5 @@
 require("index.html");
 require("stylesheets/style.scss");
-require("_config.yaml")
 
 import React from "react";
 import ReactDOM from "react-dom";
--- webpack.config.js
+++ webpack.config.js
@@ -29,15 +29,6 @@ module.exports = {
         }]
       },
       {
-        test: /\.yaml/,
-        use: [{
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]"
-          }
-        }]
-      },
-      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
